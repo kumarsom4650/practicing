792b952570de5b9cf5684922c3626bc3c7436c1b
kumarsom4650 <kumarsom4650@gmail.com>
2024-09-20 21:02:51 +0530

    let see this

--- index_1.js
+++ index_1.js
@@ -1,1 +1,14 @@
 console.log("hallo");
+// echo "# practice" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/kumarsom4650/practice.git
+// git push -u origin main
+
+
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/kumarsom4650/practice.git
+// git branch -M main
+// git push -u origin main
